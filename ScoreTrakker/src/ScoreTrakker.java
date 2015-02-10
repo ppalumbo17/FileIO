fcbc523903d3b00cc6dbf1263a246742ed962bbe
Peter Palumbo <ppalumbo@mines.edu>
2015-02-09 23:43:48 -0700

    Part III complete

--- ScoreTrakker/src/ScoreTrakker.java
+++ ScoreTrakker/src/ScoreTrakker.java
@@ -3,38 +3,46 @@ import java.io.*;
 public class ScoreTrakker {
 
 	private ArrayList<Student> students; // = new ArrayList<Student>();
-	private String[] files = {"scores.txt", "badscore.txt","nofile.txt"};
+	private String[] files = {"scores.txt", "badscore.txt","nofile.txt", "badname.txt"};
 
-	public void loadDataFromFile(String filename) throws FileNotFoundException
+	public void loadDataFromFile(String filename) throws Exception
 	{
 		students = new ArrayList<Student>();
 		FileReader reader = new FileReader(filename);	
 		Scanner in = new Scanner(reader);
 		String score = null;
 		String name = null;
+		//String first = null;
+		//String last = null;
 		//int scor = 0;
 		while(in.hasNext()){
 			
-			String first = in.next();
-			String last = in.next();
-			name = first + " " + last;
+			name = in.nextLine();
+			//try{
+			//first = in.next();
+			//last = in.next();
+			//name = first + " " + last;
+			//}
+			if(name.indexOf(" ")== -1){
+				throw new Exception(name + " does not include a first and last name.");
+				}
+						
+							
 			try{
-			score = in.next();
+			score = in.nextLine();
 			int scor = Integer.parseInt(score);
 			students.add(new Student(name, scor));
 			}
-			catch(NumberFormatException e){
+			catch(NumberFormatException n){
 				System.out.println("Error, non numeric value for " + name +" score: "+ score + " must be number. \n");
 				name = null;
 				//scor = 0;
 				
 			}
-			
-		}
-		
+		}	
 		in.close();
+	
 	}
-
 	public void printInOrder(){
 		Collections.sort(students);
 		for(Student o: students){
@@ -42,19 +50,23 @@ public class ScoreTrakker {
 		}
 	}
 
-	public void processFiles() throws FileNotFoundException{
+	public void processFiles() throws Exception{
 		//String scores = "scores.txt";
 		for(String s: files){
 		try{
 			loadDataFromFile(s);
 			printInOrder();
 		}
-		catch(FileNotFoundException e){
+		catch(FileNotFoundException f){
 			System.out.println("Cannot find specified file. \n");
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage());
 		}
 		}
 	}
-	public static void main(String[] args) throws FileNotFoundException{
+	
+	public static void main(String[] args) throws Exception{
 		ScoreTrakker score = new ScoreTrakker();
 		score.processFiles();
 	}
